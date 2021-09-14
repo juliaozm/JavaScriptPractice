83c21777e6a8b577fc3a55d562c0b96296e9a0c6
juliaozm <62850656+juliaozm@users.noreply.github.com>
2021-09-14 14:13:48 +0300

    JS 1

---
+++ js_1.js
@@ -0,0 +1,14 @@
+//camelCase
+let nullNumber = 0; 
+
+//snake_case
+let first_number = 1;
+
+//PascalCase 
+let SecondNumber = 2;
+
+alert(nullNumber);
+console.log(first_number);
+
+//alert выводит модальное окно, которое надо закрыть, чтобы продолжить работу в браузере. 
+//console.log выводит значение в консоль
